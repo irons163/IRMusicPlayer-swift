a93579dd790323af192558facf54016311db521f
Phil <phil@PhildeMac-mini.engenius.login>
2020-09-30 18:29:47 +0800

    First commit.

---
+++ IRMusicPlayer-swift/IRMusicPlayer_swift.h
@@ -0,0 +1,19 @@
+//
+//  IRMusicPlayer_swift.h
+//  IRMusicPlayer-swift
+//
+//  Created by Phil on 2020/9/30.
+//  Copyright © 2020 Phil. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IRMusicPlayer_swift.
+FOUNDATION_EXPORT double IRMusicPlayer_swiftVersionNumber;
+
+//! Project version string for IRMusicPlayer_swift.
+FOUNDATION_EXPORT const unsigned char IRMusicPlayer_swiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IRMusicPlayer_swift/PublicHeader.h>
+
+
---
+++ demo/demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  demo
+//
+//  Created by Phil on 2020/9/30.
+//  Copyright © 2020 Phil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
